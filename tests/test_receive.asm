1d07b4f1927338c266c15cbcab09842281f43c37
Veikko Sariola <veikko.sariola@gmail.com>
2020-10-26 12:53:17 +0200

    Change the .asm song format so it's a bit easier to parse, by replacing PORT and GLOBALPORT macros with LOCALPORT and GLOBALPORT.
    The LOCALPORT and GLOBALPORT just get numeric parameters (unit, port) and (voice, unit, port), respectively, which should be now quite intuitive as most of the time the port index is one of the parameters visible in the .asm file. Only a few units have extra ports beyond transformed variables. Overall, this should make the parsing of the .asm files a lot easier.

--- tests/test_receive.asm
+++ tests/test_receive.asm
@@ -13,10 +13,10 @@ END_TRACKS
 BEGIN_PATCH
     BEGIN_INSTRUMENT VOICES(1) ; Instrument0
         SU_LOADVAL MONO,VALUE(32)  ; should receive -0.5
-        SU_SEND    MONO,AMOUNT(128),PORT(5,receive,left) ; should send -0.25  
-        SU_SEND    MONO,AMOUNT(128),PORT(6,receive,left) + SEND_POP ; should send -0.25   
+        SU_SEND    MONO,AMOUNT(128),LOCALPORT(5,0) ; should send -0.25  
+        SU_SEND    MONO,AMOUNT(128),LOCALPORT(6,0) + SEND_POP ; should send -0.25   
         SU_LOADVAL MONO,VALUE(128) ; should receive 1
-        SU_SEND    MONO,AMOUNT(128),PORT(6,receive,left) + SEND_POP ; should send 0.5
+        SU_SEND    MONO,AMOUNT(128),LOCALPORT(6,0) + SEND_POP ; should send 0.5
         SU_RECEIVE MONO ; should receive -0.5
         SU_RECEIVE MONO ; should receive 0.5
         SU_OUT      STEREO,GAIN(128)
--- tests/test_send.asm
+++ tests/test_send.asm
@@ -13,10 +13,10 @@ END_TRACKS
 BEGIN_PATCH
     BEGIN_INSTRUMENT VOICES(1) ; Instrument0
         SU_LOADVAL MONO,VALUE(32)  ; should receive -0.5
-        SU_SEND     MONO,AMOUNT(96),PORT(5,load_val,value) ; should send -0.25  
-        SU_SEND     MONO,AMOUNT(96),PORT(6,load_val,value) + SEND_POP ; should send -0.25   
+        SU_SEND     MONO,AMOUNT(96),LOCALPORT(5,0) ; should send -0.25  
+        SU_SEND     MONO,AMOUNT(96),LOCALPORT(6,0) + SEND_POP ; should send -0.25   
         SU_LOADVAL MONO,VALUE(128) ; should receive 1
-        SU_SEND     MONO,AMOUNT(96),PORT(6,load_val,value) + SEND_POP ; should send 0.5
+        SU_SEND     MONO,AMOUNT(96),LOCALPORT(6,0) + SEND_POP ; should send 0.5
         SU_LOADVAL MONO,VALUE(64) ; should receive -0.5
         SU_LOADVAL MONO,VALUE(64) ; should receive 0.5
         SU_OUT      STEREO,GAIN(128)
