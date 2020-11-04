d99cfd92d930622caa1883e9a13ea3ecd066ef91
Veikko Sariola <veikko.sariola@gmail.com>
2020-11-04 23:11:51 +0200

    Change LOCALPORT and GLOBALPORT macros so that SU_SEND commands are easier to parse.
    The new format is to give either 5 or 6 parameters to SU_SEND, corresponding to local and global send, respectively. For example, a global send:
            SU_SEND     MONO,AMOUNT(128),VOICE(2),UNIT(0),PORT(1),FLAGS(SEND_POP)
    
    The SU_SEND macro does the address packing into word.

--- tests/test_receive.asm
+++ tests/test_receive.asm
@@ -13,10 +13,10 @@ END_TRACKS
 BEGIN_PATCH
     BEGIN_INSTRUMENT VOICES(1) ; Instrument0
         SU_LOADVAL MONO,VALUE(32)  ; should receive -0.5
-        SU_SEND    MONO,AMOUNT(128),LOCALPORT(5,0) ; should send -0.25  
-        SU_SEND    MONO,AMOUNT(128),LOCALPORT(6,0) + SEND_POP ; should send -0.25   
+        SU_SEND    MONO,AMOUNT(128),UNIT(5),PORT(0),FLAGS(NONE) ; should send -0.25  
+        SU_SEND    MONO,AMOUNT(128),UNIT(6),PORT(0),FLAGS(SEND_POP) ; should send -0.25   
         SU_LOADVAL MONO,VALUE(128) ; should receive 1
-        SU_SEND    MONO,AMOUNT(128),LOCALPORT(6,0) + SEND_POP ; should send 0.5
+        SU_SEND    MONO,AMOUNT(128),UNIT(6),PORT(0),FLAGS(SEND_POP) ; should send 0.5
         SU_RECEIVE MONO ; should receive -0.5
         SU_RECEIVE MONO ; should receive 0.5
         SU_OUT      STEREO,GAIN(128)
