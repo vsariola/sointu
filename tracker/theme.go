f8103af43c38d461da5fffeb5d1ae86b2cac2e9e
vsariola <vsariola@users.noreply.github.com>
2021-01-07 14:31:50 +0200

    feat(tracker): adjust color theme to purple

--- tracker/theme.go
+++ tracker/theme.go
@@ -11,9 +11,9 @@ import (
 var fontCollection []text.FontFace = gofont.Collection()
 var textShaper = text.NewCache(fontCollection)
 
-var neutral = color.RGBA{R: 73, G: 117, B: 130, A: 255}
-var light = color.RGBA{R: 138, G: 219, B: 243, A: 255}
-var dark = color.RGBA{R: 24, G: 40, B: 44, A: 255}
+var neutral = color.RGBA{R: 64, G: 39, B: 132, A: 255}
+var light = color.RGBA{R: 117, G: 75, B: 234, A: 255}
+var dark = color.RGBA{R: 25, G: 15, B: 51, A: 255}
 var white = color.RGBA{R: 255, G: 255, B: 255, A: 255}
 var blue = color.RGBA{R: 127, G: 127, B: 255, A: 255}
 var gray = color.RGBA{R: 127, G: 127, B: 127, A: 255}
