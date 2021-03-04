c7448699b61ac3d6e71ace8dd5f2fc53bb2906a8
vsariola <5684185+vsariola@users.noreply.github.com>
2021-03-04 11:18:59 +0200

    fix(gioui): disable file load / save dialogs in browser
    This at least gets the program to compile into js as sqweek does not work in browser, but we really need to look for a long term solution. How to load & save songs in browser?

---
+++ tracker/gioui/files_js.go
@@ -0,0 +1,9 @@
+package gioui
+
+func (t *Tracker) LoadSongFile() {
+	// TODO: how to load songs in browser, just disabling it for now
+}
+
+func (t *Tracker) SaveSongFile() {
+	// TODO: how to save songs in browser, just disabling it for now
+}
