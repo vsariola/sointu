ced6fa0efc4de85e327bf7ad5db3b677fafb664c
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-03-25 22:00:24 +0100

    use temp song file

--- testwasm.mjs
+++ testwasm.mjs
@@ -1,15 +1,10 @@
 import { readFile, writeFile } from 'fs/promises';
 
-const wasm = (await readFile('./groove.wasm'));
+const wasm = (await readFile('./temp_song_file.wasm'));
 
 const mod = await WebAssembly.instantiate(wasm, {
-    m: {
-        pow: Math.pow,
-        log2: Math.log2,
-        sin: Math.sin
-    }
+    
 });
 
 const mem = mod.instance.exports.m;
-
 await writeFile('test.raw', new Uint8Array(mem.buffer,mod.instance.exports.s,mod.instance.exports.l));
