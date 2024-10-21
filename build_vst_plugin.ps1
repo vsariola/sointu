e4e763e5437e00f2c085bd94e3d2cd8d9949c5d6
qm210 <quantenmace@gmail.com>
2024-10-22 01:26:16 +0200

    VST DLLs v1.0 in both synth versions (seem identical)

--- build_vst_plugin.ps1
+++ build_vst_plugin.ps1
@@ -1,14 +1,15 @@
-  if ($args -contains "native") {
+# Specify "native" or "go" if you only want one VST version.
+
+if ($args -notcontains "go") {
 
     Write-Host "Build VST with ASM synth"
-    go build -buildmode=c-shared -tags="plugin","native" -o sointu-vsti.dll .\cmd\sointu-vsti\
+    go build -buildmode=c-shared -tags="plugin","native" -o sointu-vsti-native.dll .\cmd\sointu-vsti\
 
-  } elseif ($args -contains "go") {
+}
+if ($args -notcontains "native") {
 
     Write-Host "Build VST with GO synth"
-    go build -buildmode=c-shared -tags="plugin" -o sointu-vsti.dll .\cmd\sointu-vsti\
+    go build -buildmode=c-shared -tags="plugin" -o sointu-vsti-go.dll .\cmd\sointu-vsti\
 
-  } else {
-    Write-Host "specify either ""native"" or ""go"" argument."
-  }
+}
 
