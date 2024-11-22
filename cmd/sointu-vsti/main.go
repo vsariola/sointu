0534ec4e85a2ad717b1afe409c319cafb26fcb1c
qm210 <quantenmace@gmail.com>
2024-11-22 13:20:35 +0100

    fix

--- cmd/sointu-vsti/main.go
+++ cmd/sointu-vsti/main.go
@@ -109,7 +109,7 @@ func init() {
 						buf = append(buf, make(sointu.AudioBuffer, out.Frames-len(buf))...)
 					}
 					buf = buf[:out.Frames]
-					player.Process(buf, &context, nil)
+					player.Process(buf, &context)
 					for i := 0; i < out.Frames; i++ {
 						left[i], right[i] = buf[i][0], buf[i][1]
 					}
